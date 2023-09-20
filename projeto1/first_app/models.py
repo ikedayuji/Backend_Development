8f0c4ff84997b10f4742e4c6a32e7df42e7a5aef
Eric <93358246+ikedayuji@users.noreply.github.com>
2023-09-19 23:53:02 -0300

    projeto1

---
+++ projeto1/first_app/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+class Topic(models.Model):
+    name = models.CharField(max_length=100)
+
+class Webpage(models.Model):
+    name = models.CharField(max_length=264, unique=True)
+    url = models.URLField(unique=True)
+
+    def __str__(self):
+        return self.name
+    
+class AccessRecord(models.Model):  
+    name = models.ForeignKey(Webpage, on_delete=models.CASCADE)
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.date)
+    
+class Movel(models.Model):
+    name = models.CharField(max_length=100)
+    preco = models.DecimalField(decimal_places=2, max_digits=10)
+
+    def __str__(self):
+        return self.name
+    
+class Filme(models.Model):
+    titulo = models.CharField(max_length=256)
+    duracao = models.PositiveIntegerField() 
+    ano_lancamento = models.PositiveIntegerField()
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=256)
+    sobrenome = models.CharField(max_length=256)
+    telefone = models.PositiveIntegerField()
---
+++ projeto1/first_app/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import ForName
+from .forms import movelform
+
+
+def index(request):
+    return render(request, "index.html")
+
+def calculadora(request):
+    return render(request, "calculadora.html")
+
+def parana(request):
+    return render(request, "parana.html")
+
+def formulario_simples(request):
+    form = ForName()
+    
+    if request.method == 'POST':
+        
+        form = form.FormName(request.POST)
+        
+        if form.is_valid():
+            
+            print("Form Validation Sucess. Prints in console")
+            print("Name"+form.cleaned_data['name'])
+            print("Email"+form.cleaned_data['email'])
+            print("Text"+form.cleaned_data['text'])
+    return render(request,'movel.html',{'form':form})
+
+def formulario_movel(request):
+    form = movelform()
+    
+    if request.method == 'POST':
+        
+        form = movelform(request.POST)
+        if form.is_valid():
+            form.save()
+        else:
+            print("Deu erro")
+            
+    return render(request,'movel.html',{'form':form})
+
+        
+    
