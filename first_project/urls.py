342146969166e45f32dec5338de2dc3f64d31344
ikedayuji <93358246+ikedayuji@users.noreply.github.com>
2023-09-20 11:04:33 -0300

    Atividade Aula 06 19/09/2023

--- first_project/urls.py
+++ first_project/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
 
 if settings.DEBUG:
     import debug_toolbar
-    url patterns = [
+    urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls))
     ] + urlpatterns 
